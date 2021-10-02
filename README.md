b28f95cc4d4376d2e857f48d2fecdd141daf6767
Giovanni <g.salmeri@mondodomani.org>
2021-10-02 23:14:00 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# yellow-cookieconsent
-Cookie consent banner for web analytics.
+Cookieconsent 0.8.16
+=================
+Web analytics and cookie consent banner.
+
+<p align="center"><img src="cookieconsent-screenshot.png?raw=true" width="795" height="836" alt="Screenshot"></p>
+
+## How to add a cookie consent banner
+
+This extension adds Google Analytics, Facebook Pixel or Hotjar to the site and creates a GDPR-compliant cookie consent banner which enables or disables them.
+
+## Settings
+
+The following settings can be configured in file `system/extensions/yellow-system.ini`:
+
+`CookieconsentStyle` (default: `1`) = banner style; possible values are `1`, `2`, `3`  
+`CookieconsentPolicy` = page for policy link, e.g. `privacy-statement` 
+`CookieconsentGoogleAnalytics` = Google Analytics alphanumeric tracking code  
+`CookieconsentFacebookPixel` = Facebook Pixel numeric code  
+`CookieconsentHotjar` = Hotjar numeric tracking code
+
+Al least one of `CookieconsentGoogleAnalytics`, `CookieconsentFacebookPixel` or `CookieconsentHotjar` must be set for the extension to be useful.
+
+## Installation
+
+[Download extension](https://github.com/GiovanniSalmeri/yellow-cookieconsent/archive/master.zip) and copy zip file into your `system/extensions` folder. Right click if you use Safari.
+
+This extension uses [GlowCookies](https://manucaralmo.github.io/glow-cookies-web/) by Manuel Carrillo Almoguera.
+
+## Developer
+
+Giovanni Salmeri. [Get help](https://github.com/GiovanniSalmeri/yellow-metatags/issues).
---
+++ cookieconsent.js
@@ -0,0 +1,282 @@
+/*
+    GLOW COOKIES
+    CREATED BY MANUEL CARRILLO
+    https://github.com/manucaralmo/GlowCookies
+    2021 - v 3.1.3
+
+Changes by GS 2021-10-02
+
++ Corrected according to https://github.com/manucaralmo/GlowCookies/discussions/40
++ Deleted SVG
++ Deleted addCss
++ Deleted languages
+
+*/
+
+class GlowCookies {
+  constructor() {
+    // Cookies banner
+    this.banner = undefined
+    // Config
+    this.config = undefined
+    this.tracking = undefined
+    // DOM ELEMENTS
+    this.PreBanner = undefined
+    this.Cookies = undefined
+    this.DOMbanner = undefined
+  }
+
+  render() {
+    this.createDOMElements()
+    this.checkStatus()
+  }
+
+  createDOMElements() {
+    // COOKIES BUTTON
+    this.PreBanner = document.createElement("div");
+    this.PreBanner.innerHTML = `<button type="button" id="prebannerBtn" class="prebanner prebanner__border__${this.config.bannerStyle} glowCookies__${this.config.position} glowCookies__${this.config.border} animation" style="color: ${this.banner.manageCookies.color}; background-color: ${this.banner.manageCookies.background};">${this.banner.manageCookies.text}</button>`;
+    this.PreBanner.style.display = "none";
+    document.body.appendChild(this.PreBanner);
+
+    // COOKIES BANNER
+    this.Cookies = document.createElement("div");
+    this.Cookies.innerHTML = `<div 
+                                    id="glowCookies-banner" 
+                                    class="glowCookies__banner glowCookies__banner__${this.config.bannerStyle} glowCookies__${this.config.border} glowCookies__${this.config.position}"
+                                    style="background-color: ${this.banner.background};"
+                                >
+                                    <h3 style="color: ${this.banner.color};">${this.banner.heading}</h3>
+                                    <p style="color: ${this.banner.color};">
+                                        ${this.banner.description} 
+                                        <a 
+                                            href="${this.banner.link}"
+                                            target="_blank" 
+                                            class="read__more"
+                                            style="color: ${this.banner.color};"
+                                        >
+                                            ${this.banner.linkText}
+                                        </a>
+                                    </p>
+                                    <div class="btn__section">
+                                        <button type="button" id="acceptCookies" class="btn__accept accept__btn__styles" style="color: ${this.banner.acceptBtn.color}; background-color: ${this.banner.acceptBtn.background};">
+                                            ${this.banner.acceptBtn.text}
+                                        </button>
+                                        <button type="button" id="rejectCookies" class="btn__settings settings__btn__styles" style="color: ${this.banner.rejectBtn.color}; background-color: ${this.banner.rejectBtn.background};">
+                                            ${this.banner.rejectBtn.text}
+                                        </button>
+                                    </div>
+                                </div>
+                            `;
+    document.body.appendChild(this.Cookies);
+    this.DOMbanner = document.getElementById('glowCookies-banner')
+
+
+    // SET EVENT LISTENERS
+    document.getElementById('prebannerBtn').addEventListener('click', () => this.openSelector())
+    document.getElementById('acceptCookies').addEventListener('click', () => this.acceptCookies())
+    document.getElementById('rejectCookies').addEventListener('click', () => this.rejectCookies())
+  }
+
+  checkStatus() {
+    switch (localStorage.getItem("GlowCookies")) {
+      case "1":
+        this.openManageCookies();
+        this.activateTracking();
+        this.addCustomScript();
+        break;
+      case "0":
+        this.openManageCookies();
+        break;
+      default:
+        this.openSelector();
+    }
+  }
+
+  openManageCookies() {
+    this.PreBanner.style.display = this.config.hideAfterClick ? "none" : "block"
+    this.DOMbanner.classList.remove('glowCookies__show')
+  }
+
+  openSelector() {
+    this.PreBanner.style.display = "none";
+    this.DOMbanner.classList.add('glowCookies__show')
+  }
+
+  acceptCookies() {
+    localStorage.setItem("GlowCookies", "1")
+    this.openManageCookies()
+    this.activateTracking()
+    this.addCustomScript()
+  }
+
+  rejectCookies() {
+    localStorage.setItem("GlowCookies", "0");
+    this.openManageCookies();
+    this.disableTracking();
+  }
+
+  activateTracking() {
+    // Google Analytics Tracking
+    if (this.tracking.AnalyticsCode) {
+      let Analytics = document.createElement('script');
+      Analytics.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${this.tracking.AnalyticsCode}`);
+      document.head.appendChild(Analytics);
+      let AnalyticsData = document.createElement('script');
+      AnalyticsData.text = `window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${this.tracking.AnalyticsCode}');`;
+      document.head.appendChild(AnalyticsData);
+    }
+
+    // Facebook pixel tracking code
+    if (this.tracking.FacebookPixelCode) {
+      let FacebookPixelData = document.createElement('script');
+      FacebookPixelData.text = `
+                                    !function(f,b,e,v,n,t,s)
+                                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                                    n.queue=[];t=b.createElement(e);t.async=!0;
+                                    t.src=v;s=b.getElementsByTagName(e)[0];
+                                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                    'https://connect.facebook.net/en_US/fbevents.js');
+                                    fbq('init', '${this.tracking.FacebookPixelCode}');
+                                    fbq('track', 'PageView');
+                                `;
+      document.head.appendChild(FacebookPixelData);
+      let FacebookPixelNS = document.createElement('noscript');
+      let FacebookPixel = document.createElement('img');
+      FacebookPixel.setAttribute('height', `1`);
+      FacebookPixel.setAttribute('width', `1`);
+      FacebookPixel.setAttribute('style', `display:none`);
+      FacebookPixel.setAttribute('src', `https://www.facebook.com/tr?id=${this.tracking.FacebookPixelCode}&ev=PageView&noscript=1`);
+      FacebookPixelNS.appendChild(FacebookPixel);
+      document.head.appendChild(FacebookPixelNS);
+    }
+
+    // Hotjar Tracking
+    if (this.tracking.HotjarTrackingCode) {
+      let hotjarTrackingData = document.createElement('script');
+      hotjarTrackingData.text = `
+                                (function(h,o,t,j,a,r){
+                                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                                    h._hjSettings={hjid:${this.tracking.HotjarTrackingCode},hjsv:6};
+                                    a=o.getElementsByTagName('head')[0];
+                                    r=o.createElement('script');r.async=1;
+                                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                                    a.appendChild(r);
+                                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                                `;
+      document.head.appendChild(hotjarTrackingData);
+    }
+  }
+
+  disableTracking() {
+    // Google Analytics Tracking ('client_storage': 'none')
+    if (this.tracking.AnalyticsCode) {
+      let Analytics = document.createElement('script');
+      Analytics.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${this.tracking.AnalyticsCode}`);
+      document.head.appendChild(Analytics);
+      let AnalyticsData = document.createElement('script');
+      AnalyticsData.text = `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${this.tracking.AnalyticsCode}' , {
+                            'client_storage': 'none',
+                            'anonymize_ip': true
+                        });`;
+      document.head.appendChild(AnalyticsData);
+    }
+
+    // Clear cookies - not working 100%
+    this.clearCookies()
+  }
+
+  clearCookies() {
+    let cookies = document.cookie.split("; ");
+    for (let c = 0; c < cookies.length; c++) {
+      let d = window.location.hostname.split(".");
+      while (d.length > 0) {
+        let cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+        let p = location.pathname.split('/');
+        document.cookie = cookieBase + '/';
+        while (p.length > 0) {
+          document.cookie = cookieBase + p.join('/');
+          p.pop();
+        };
+        d.shift();
+      }
+    }
+  }
+
+  addCustomScript() {
+    if (this.tracking.customScript !== undefined) {
+      let customScriptTag
+
+      this.tracking.customScript.forEach(script => {
+        if (script.type === 'src') {
+          customScriptTag = document.createElement('script');
+          customScriptTag.setAttribute('src', script.content);
+        } else if (script.type === 'custom') {
+          customScriptTag = document.createElement('script');
+          customScriptTag.text = script.content;
+        }
+
+        if (script.position === 'head') {
+          document.head.appendChild(customScriptTag);
+        } else {
+          document.body.appendChild(customScriptTag);
+        }
+      })
+    }
+  }
+
+  start(languaje, obj) {
+    if (!obj) obj = {}
+    //const lang = new LanguagesGC(languaje)
+
+    this.config = {
+      border: obj.border || 'border',
+      position: obj.position || 'left',
+      hideAfterClick: obj.hideAfterClick || false,
+      bannerStyle: obj.style || 2
+    }
+
+    this.tracking = {
+      AnalyticsCode: obj.analytics || undefined,
+      FacebookPixelCode: obj.facebookPixel || undefined,
+      HotjarTrackingCode: obj.hotjar || undefined,
+      customScript: obj.customScript || undefined
+    }
+
+    this.banner = {
+      description: obj.bannerDescription || lang.bannerDescription,
+      linkText: obj.bannerLinkText || lang.bannerLinkText,
+      link: obj.policyLink || '#link',
+      background: obj.bannerBackground || '#fff',
+      color: obj.bannerColor || '#1d2e38',
+      heading: obj.bannerHeading !== 'none' ? obj.bannerHeading || lang.bannerHeading : '',
+      acceptBtn: {
+        text: obj.acceptBtnText || lang.acceptBtnText,
+        background: obj.acceptBtnBackground || '#253b48',
+        color: obj.acceptBtnColor || '#fff'
+      },
+      rejectBtn: {
+        text: obj.rejectBtnText || lang.rejectBtnText,
+        background: obj.rejectBtnBackground || '#E8E8E8',
+        color: obj.rejectBtnColor || '#636363'
+      },
+      manageCookies: {
+        color: obj.manageColor || '#1d2e38',
+        background: obj.manageBackground || '#fff',
+        text: obj.manageText || lang.manageText,
+      }
+    }
+
+    // Draw banner
+    window.addEventListener('load', () => { this.render() })
+  }
+}
+
+const glowCookies = new GlowCookies()
